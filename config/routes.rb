287314b8b93a40a59a50dbb72f1a3aa719021662
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2018-08-09 09:49:49 +0700

    like post

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: %i(index show)
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
-    resources :likes
+    resources :likes, shallow: true
   end 
 end
